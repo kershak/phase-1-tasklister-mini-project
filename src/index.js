4ee7e3efe3ef1e11648722cb0880087273b465f0
Nills <yakusaz17@gmail.com>
2024-08-19 02:56:16 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForce = document.querySelector("form");
+
+  newTaskForce.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const newToDo = e.target.querySelector("#new-task-description").value
+
+    buildToDo(newToDo);
+
+    newTaskForce.reset();
+  })
 });
+
+function buildToDo(newToDo) {
+  const taskList = document.createElement ("li");
+  const deleteButton = document.createElement ("button");
+
+  deteleButton.textContent = "x";
+
+  taskList.textContent = newToDo;
+
+  taskList.appendChild(deleteButton);
+
+  document.querySelector("#tasks").appendChild(taskList);
+  
+  deleteButton.addEventListener("click", handleDelete);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
